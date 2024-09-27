924ebf331e8099f8ee0ccf7501ea2db2027d39e7
Aditya Kitukale <adityakitukale4599@gmail.com>
2024-09-27 23:20:55 +0530

    added commit

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://delightful-chimera-654345.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://delightful-chimera-654345.netlify.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
